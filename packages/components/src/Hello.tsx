4c3913dc9919ed2cab66b17e1b88e9d59094b509
John Tucker <john.tucker@sharpspring.com>
2018-04-12 06:13:52 -0400

    antd in components

--- packages/components/src/Hello.tsx
+++ packages/components/src/Hello.tsx
@@ -1,4 +1,10 @@
-import React from "react";
+import { Button } from 'antd';
+import React from 'react';
 
-const Hello = () => <div>Hello World</div>;
+const Hello = () => (
+  <div>
+    Hello World
+    <Button type="primary">Test</Button>
+  </div>
+);
 export default Hello;
--- packages/components/webpack.config.js
+++ packages/components/webpack.config.js
@@ -1,5 +1,6 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
+const tsImportPluginFactory = require('ts-import-plugin');
 
 module.exports = {
   entry: {
@@ -17,12 +18,40 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'ts-loader',
+          options: {
+            getCustomTransformers: () => ({
+              before: [tsImportPluginFactory({
+                libraryName: 'antd',
+                libraryDirectory: 'es',
+                style: true,
+              })],
+            }),
+            onlyCompileBundledFiles: true,
+          },
         },
+      },
+      {
+        test: /node_modules\/.*\.(css|less)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'less-loader',
+          },
+        ],
       },
     ],
   },
   externals: [
     'react',
+    /antd\/.*/,
   ],
   plugins: [
     new CleanWebpackPlugin(['lib']),
