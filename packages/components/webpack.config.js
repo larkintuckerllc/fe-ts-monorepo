c2159006bf85dc9537743df381a25b64db0823d4
John Tucker <john.tucker@sharpspring.com>
2018-04-13 08:52:46 -0400

    Reverted but broken

--- packages/components/webpack.config.js
+++ packages/components/webpack.config.js
@@ -2,98 +2,103 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
 const tsImportPluginFactory = require('ts-import-plugin');
 
-module.exports = {
-  entry: {
-    Hello: './src/Hello/Hello.tsx',
-  },
-  output: {
-    path: path.resolve(__dirname, 'lib'),
-    filename: '[name].js',
-    libraryTarget: 'commonjs2',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(tsx?)$/,
-        enforce: 'pre',
-        exclude: /node_modules/,
-        loader: 'tslint-loader',
-        options: {
-          failOnHint: true,
-        },
-      },
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader',
+const OUTPUT_PATH = 'lib';
+module.exports = (env) => {
+  const plugins = [];
+  if (env.NODE_ENV === 'production') {
+    plugins.push(new CleanWebpackPlugin([OUTPUT_PATH]));
+  }
+  return ({
+    entry: {
+      Hello: './src/Hello/Hello.tsx',
+    },
+    output: {
+      path: path.resolve(__dirname, OUTPUT_PATH),
+      filename: '[name].js',
+      libraryTarget: 'commonjs2',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(tsx?)$/,
+          enforce: 'pre',
+          exclude: /node_modules/,
+          loader: 'tslint-loader',
           options: {
-            getCustomTransformers: () => ({
-              before: [tsImportPluginFactory({
-                libraryName: 'antd',
-                libraryDirectory: 'lib',
-                style: true,
-              })],
-            }),
-            onlyCompileBundledFiles: true,
+            failOnHint: true,
           },
         },
-      },
-      {
-        test: /node_modules\/.*\.(css|less)$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'ts-loader',
             options: {
-              importLoaders: 1,
+              getCustomTransformers: () => ({
+                before: [tsImportPluginFactory({
+                  libraryName: 'antd',
+                  libraryDirectory: 'lib',
+                  style: true,
+                })],
+              }),
+              onlyCompileBundledFiles: true,
             },
           },
-          {
-            loader: 'less-loader',
-          },
-        ],
-      },
-      {
-        test: /\.(css|less)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'typings-for-css-modules-loader',
-            options: {
-              importLoaders: 2,
-              modules: true,
-              namedExport: true,
+        },
+        {
+          test: /node_modules\/.*\.(css|less)$/,
+          use: [
+            {
+              loader: 'style-loader',
             },
-          },
-          {
-            loader: 'postcss-loader',
-          },
-          {
-            loader: 'less-loader',
-          },
-        ],
-      },
-      {
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        loader: 'url-loader',
-      },
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+              },
+            },
+            {
+              loader: 'less-loader',
+            },
+          ],
+        },
+        {
+          test: /\.(css|less)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'style-loader',
+            },
+            {
+              loader: 'typings-for-css-modules-loader',
+              options: {
+                importLoaders: 2,
+                modules: true,
+                namedExport: true,
+              },
+            },
+            {
+              loader: 'postcss-loader',
+            },
+            {
+              loader: 'less-loader',
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+          loader: 'url-loader',
+        },
+      ],
+    },
+    externals: [
+      'react',
+      /antd\/.*/,
     ],
-  },
-  externals: [
-    'react',
-    /antd\/.*/,
-  ],
-  plugins: [
-    new CleanWebpackPlugin(['lib']),
-  ],
-  resolve: {
-    extensions: ['.js', '.json', '.ts', '.tsx'],
-    symlinks: false,
-  },
+    plugins,
+    resolve: {
+      extensions: ['.js', '.json', '.ts', '.tsx'],
+      symlinks: false,
+    },
+  });
 };
